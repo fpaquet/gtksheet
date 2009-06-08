12e0b838d2b5a533587ba5f6bc395652b64532f8
victor <victor@minino.(none)>
2009-06-08 11:05:43 +0200

    Update test

--- gtksheet/testgtksheet.c
+++ gtksheet/testgtksheet.c
@@ -3,40 +3,13 @@
 #include <gtk/gtk.h>
 #include <gdk/gdk.h>
 #include <gdk/gdkkeysyms.h>
+#include <pango/pango.h>
 #include <glib.h>
 #include <string.h>
 #include "gtksheet.h"
 
-
-#define DEFAULT_PRECISION 3
-#define DEFAULT_SPACE 8
-#define NUM_SHEETS 3 
-
 GtkWidget *window;
-GtkWidget *main_vbox;
-GtkWidget *notebook;
-GtkWidget **sheets;
-GtkWidget **scrolled_windows;
-GtkWidget *show_hide_box;
-GtkWidget *status_box;
-GtkWidget *location;
-GtkWidget *entry;
-GtkWidget *fgcolorcombo;
-GtkWidget *bgcolorcombo;
-GtkWidget *bordercombo;
-GdkPixmap *pixmap;
-GdkBitmap *mask;
-GtkWidget *bg_pixmap;
-GtkWidget *fg_pixmap;
-GtkWidget *toolbar;
-GtkWidget *left_button;
-GtkWidget *center_button;
-GtkWidget *right_button;
-GtkWidget *tpixmap;
-GtkWidget *bullet[10];
-GtkWidget *smile;
-GtkWidget *curve;
-GtkWidget *popup;
+GtkWidget *colorbutton;
 
 void
 quit ()
@@ -44,27 +17,109 @@ quit ()
   gtk_main_quit();
 }
 
+gint
+activate_sheet_cell_cb(GtkWidget *widget, gint row, gint column, gpointer data) 
+{
+    g_return_val_if_fail(GTK_IS_SHEET(widget), FALSE);
+
+    GtkSheet *sheet;
+    const gchar *text;
+
+    sheet=GTK_SHEET(widget);
+
+    text = gtk_sheet_cell_get_text(sheet, row, column);
+    if (text)
+    {
+        g_message(text);
+    }
+}
+
+void
+bg_color_changed_cb(GtkWidget *widget, gpointer data) 
+{
+    g_return_if_fail(GTK_IS_SHEET(data));
+    g_return_if_fail(GTK_IS_COLOR_BUTTON(widget));
+
+    GtkColorButton *colorbtn = GTK_COLOR_BUTTON(widget);
+    GtkSheet *sheet = GTK_SHEET(data);
+    GdkColor color;
+
+    /*
+    Bug in GtkSheet: the background color of a cell is not respected!!!
+    */
+    gtk_color_button_get_color(colorbtn, &color);
+    gtk_sheet_range_set_background(sheet, &(sheet->range), &color);
+    
+}
+
+void
+font_changed_cb(GtkWidget *widget, gpointer data)
+{
+    g_return_if_fail(GTK_IS_SHEET(data));
+    g_return_if_fail(GTK_IS_FONT_BUTTON(widget));
+
+    GtkSheet *sheet = GTK_SHEET(data);
+    GtkFontButton *fontbutton = GTK_FONT_BUTTON(widget);
+
+    const gchar *font_name = gtk_font_button_get_font_name(fontbutton);
+    PangoFontDescription *fd = pango_font_description_from_string(font_name);
+    if (fd)
+    {
+        gtk_sheet_range_set_font(sheet, &(sheet->range), fd);
+        pango_font_description_free(fd);
+    }
+}
+
 int main(int argc, char *argv[])
 {
     gtk_init(&argc, &argv);
-    window=gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "GtkSheet Demo");
-    gtk_widget_set_usize(GTK_WIDGET(window), 900, 600);
-
+    gtk_widget_set_size_request(window, 500, 500);
     gtk_signal_connect (GTK_OBJECT (window), "destroy",
-	          GTK_SIGNAL_FUNC (quit), NULL);
-    main_vbox = gtk_vbox_new(FALSE,1);
+	                    GTK_SIGNAL_FUNC (quit), NULL);
 
-    GtkWidget *sheet = gtk_sheet_new_with_custom_entry(1000,26, "Sheet 1", gtk_entry_get_type());
+    GdkColor bg, grid;
+    GdkColormap *colormap = gdk_colormap_get_system();
+    gdk_color_parse("light yellow", &bg);
+    gdk_color_parse("light blue", &grid);
+    gdk_colormap_alloc_color(colormap, &bg, TRUE, TRUE);
+    gdk_colormap_alloc_color(colormap, &grid, TRUE, TRUE);
+
+    GtkWidget *sheet = gtk_sheet_new(1000,26, "Sheet 1");
+    gtk_sheet_set_background(GTK_SHEET(sheet), &bg);
+    gtk_sheet_set_grid(GTK_SHEET(sheet), &grid);
+    gtk_signal_connect(GTK_OBJECT(sheet), "activate",
+                       (GtkSignalFunc)activate_sheet_cell_cb, NULL);
     GtkWidget *scrwin = gtk_scrolled_window_new(NULL, NULL);
     gtk_container_add(GTK_CONTAINER(scrwin), sheet);
+
+    colorbutton = gtk_color_button_new();
+    gtk_signal_connect(GTK_OBJECT(colorbutton), "color-set", 
+                       GTK_SIGNAL_FUNC(bg_color_changed_cb), sheet);
+
+    GtkWidget *fontbutton = gtk_font_button_new();
+    gtk_signal_connect(GTK_OBJECT(fontbutton), "font-set",
+                       GTK_SIGNAL_FUNC(font_changed_cb), sheet);
+       
+
+    GtkWidget *hbox = gtk_hbox_new(FALSE , 5);
+    gtk_box_pack_start(GTK_BOX(hbox), fontbutton, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(hbox), colorbutton, FALSE, FALSE, 0);
+
+    GtkWidget *main_vbox = gtk_vbox_new(FALSE,1);
+    gtk_box_pack_start(GTK_BOX(main_vbox), hbox, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(main_vbox), scrwin, TRUE, TRUE, 0);
+
     gtk_container_add(GTK_CONTAINER(window), main_vbox);
 
-    gtk_widget_show_all(  window );
+    gtk_widget_show_all( window );
+
 
     gtk_main();
 
 }
+
 
 

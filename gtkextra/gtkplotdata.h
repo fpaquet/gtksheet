a0af5a2ce56e906cd2516898c632dab5f269028e
Tom Schoonjans <tomschoonjans@uniss.it>
2014-01-18 17:18:28 +0000

    added two functions to header gtkplotdata.h

--- gtkextra/gtkplotdata.h
+++ gtkextra/gtkplotdata.h
@@ -274,6 +274,10 @@ void		gtk_plot_data_gradient_set_scale	(GtkPlotData *data,
 							 GtkPlotScale scale);
 void 		gtk_plot_data_gradient_set_title        (GtkPlotData *data,
                                          		 gchar *title);
+void 		gtk_plot_data_set_gradient_show_lt_gt 	(GtkPlotData *data,
+							 gboolean show);
+gboolean	gtk_plot_data_gradient_show_lt_gt 	(GtkPlotData *data);
+
 void		gtk_plot_data_set_link			(GtkPlotData *dataset,
 							 gpointer link);
 gpointer	gtk_plot_data_get_link			(GtkPlotData *dataset);
